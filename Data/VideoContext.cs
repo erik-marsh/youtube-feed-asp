184ccb381c300037bb48d01b309ad8e57d6552ca
Erik Marsh <erik.i.marsh@gmail.com>
2022-09-05 13:17:32 -0700

    Initial database context

---
+++ Data/VideoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using youtube_feed_asp.Models;
+
+namespace youtube_feed_asp.Data;
+
+public class VideoContext : DbContext
+{
+    public VideoContext(DbContextOptions<VideoContext> options)
+        : base(options)
+    {   
+    }
+
+    public DbSet<Video> Videos => Set<Video>();
+    public DbSet<Channel> Channels => Set<Channel>();
+}
