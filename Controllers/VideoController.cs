d0bb0461610bdaf5d00339c90b18e09a49aa45fe
Erik Marsh <erik.i.marsh@gmail.com>
2022-09-12 00:07:59 -0700

    various new read methods for the VideoController

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -72,9 +72,58 @@ public class VideoController : ControllerBase
         return response;
     }
 
+    [HttpGet("videos/{id}")]
+    public ActionResult<VideoResponse> GetVideo(string id)
+    {
+        var video = m_service.GetVideo(id);
+        if (video is null)
+            return NotFound(); // 404 Not Found
+        return new VideoResponse(video); // implicit 200 OK
+    }
+
+    [HttpGet("subscriptions")]
+    public IEnumerable<VideoResponse> GetAllSubscriptions()
+    {
+        var videos = m_service.GetAllVideosByType(VideoType.Subscription);
+        return videos.Select(video => new VideoResponse(video));
+    }
+
+    [HttpGet("watch-later")]
+    public IEnumerable<VideoResponse> GetAllWatchLaters()
+    {
+        var videos = m_service.GetAllVideosByType(VideoType.WatchLater);
+        return videos.Select(video => new VideoResponse(video));
+    }
+
+    // TODO: the videos within a channel response do not get turned into VideoResponses,
+    // hence the enums do not appear as text, just an int
     [HttpGet("channels")]
     public IEnumerable<Channel> GetAllChannels()
     {
         return m_service.GetAllChannels();
+    }
+
+    [HttpGet("channels/{id}")]
+    public ActionResult<Channel> GetChannel(string id)
+    {
+        var channel = m_service.GetChannel(id);
+        if (channel is null)
+            return NotFound();
+        
+        return channel;
+    }
+
+    [HttpGet("channels/{id}/subscriptions")]
+    public IEnumerable<VideoResponse> GetChannelSubscriptions(string id)
+    {
+        return m_service.GetChannelVideosByType(id, VideoType.Subscription)
+            .Select(video => new VideoResponse(video));
+    }
+
+    [HttpGet("channels/{id}/watch-later")]
+    public IEnumerable<VideoResponse> GetChannelWatchLaters(string id)
+    {
+        return m_service.GetChannelVideosByType(id, VideoType.WatchLater)
+            .Select(video => new VideoResponse(video));
     }
 }
