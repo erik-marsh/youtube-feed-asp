28824e3ce4c0ba489851f746de35d292fc3f9170
Erik Marsh <erik.i.marsh@gmail.com>
2022-09-11 20:57:58 -0700

    more sample data

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -31,6 +31,44 @@ public static class DbInitializer
             LastModified = 1000000
         };
 
+        //UCSPLhwvj0gBufjDRzSQb3GQ|BobbyBroccoli|2022-07-22 12:00:51+00:00
+        var channel2 = new Channel {
+            Id = "UCSPLhwvj0gBufjDRzSQb3GQ",
+            Name = "BobbyBroccoli",
+            LastModified = 123476372,
+            Videos = new List<Video>() {
+
+            }
+        };
+
+        var channel3 = new Channel {
+            Id = "UCLrno_gfh32wX4I8-6qV4Wg",
+            Name = "Running Shine",
+            LastModified = 1234763722,
+            Videos = new List<Video>()
+        };
+
+        var video30 = new Video {
+            Id = "LSKdQhrcbuw",
+            Uploader = channel3,
+            Title = "Kirby 64: The Crystal Shards Review",
+            TimePublished = 1928374,
+            TimeAdded = 92187398,
+            Type = VideoType.Subscription
+        };
+
+        channel3.Videos.Add(video30);
+
+        //UCBn6YYmhnruGtEtlKxYbQ9A|K Klein|2022-08-26 10:00:01+00:00
+        var channel4 = new Channel {
+            Id = "UCBn6YYmhnruGtEtlKxYbQ9A",
+            Name = "K Klein",
+            LastModified = 1234722672,
+            Videos = new List<Video>() {
+
+            }
+        };
+
         video.Uploader = channel;
 
         channel.Videos = new List<Video>();
@@ -38,6 +76,13 @@ public static class DbInitializer
         channel.Videos.Add(video2);
 
         context.Videos.Add(video);
+        context.Videos.Add(video2);
+        context.Videos.Add(video30);
+
+        context.Channels.Add(channel);
+        context.Channels.Add(channel2);
+        context.Channels.Add(channel3);
+        context.Channels.Add(channel4);
         context.SaveChanges();
     }
 }
