23a298b330beb9ce65afc9722d5bf7a2e40743f6
Erik Marsh <erik.i.marsh@gmail.com>
2022-12-29 23:55:49 -0800

    Comment out unused code for now

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -114,90 +114,90 @@ public class VideoController : ControllerBase
         return videos.Select(video => new VideoResponse(video)).ToList();
     }
 
-    [HttpGet("videos")]
-    public IEnumerable<VideoResponse> GetAllVideos()
-    {
-        // when your return a POCO (plain old CLR object) like this, it is wrapped in an ObjectResult implicitly, then serialized
-        var videos = m_service.GetAllVideos();
-        var response = new List<VideoResponse>();
-        foreach (var v in videos)
-        {
-            response.Add(new VideoResponse(v));
-        }
-        return response;
-    }
+    // [HttpGet("videos")]
+    // public IEnumerable<VideoResponse> GetAllVideos()
+    // {
+    //     // when your return a POCO (plain old CLR object) like this, it is wrapped in an ObjectResult implicitly, then serialized
+    //     var videos = m_service.GetAllVideos();
+    //     var response = new List<VideoResponse>();
+    //     foreach (var v in videos)
+    //     {
+    //         response.Add(new VideoResponse(v));
+    //     }
+    //     return response;
+    // }
 
-    [HttpGet("videos/{id}")]
-    public ActionResult<VideoResponse> GetVideo(string id)
-    {
-        var video = m_service.GetVideo(id);
-        if (video is null)
-            return NotFound(); // 404 Not Found
-        return new VideoResponse(video); // implicit 200 OK
-    }
+    // [HttpGet("videos/{id}")]
+    // public ActionResult<VideoResponse> GetVideo(string id)
+    // {
+    //     var video = m_service.GetVideo(id);
+    //     if (video is null)
+    //         return NotFound(); // 404 Not Found
+    //     return new VideoResponse(video); // implicit 200 OK
+    // }
 
-    [HttpGet("subscriptions")]
-    public IEnumerable<VideoResponse> GetAllSubscriptions()
-    {
-        var videos = m_service.GetAllVideosByType(VideoType.Subscription);
-        return videos.Select(video => new VideoResponse(video));
-    }
+    // [HttpGet("subscriptions")]
+    // public IEnumerable<VideoResponse> GetAllSubscriptions()
+    // {
+    //     var videos = m_service.GetAllVideosByType(VideoType.Subscription);
+    //     return videos.Select(video => new VideoResponse(video));
+    // }
 
-    [HttpGet("watch-later")]
-    public IEnumerable<VideoResponse> GetAllWatchLaters()
-    {
-        var videos = m_service.GetAllVideosByType(VideoType.WatchLater);
-        return videos.Select(video => new VideoResponse(video));
-    }
+    // [HttpGet("watch-later")]
+    // public IEnumerable<VideoResponse> GetAllWatchLaters()
+    // {
+    //     var videos = m_service.GetAllVideosByType(VideoType.WatchLater);
+    //     return videos.Select(video => new VideoResponse(video));
+    // }
 
-    // TODO: the videos within a channel response do not get turned into VideoResponses,
-    // hence the enums do not appear as text, just an int
-    [HttpGet("channels")]
-    public IEnumerable<Channel> GetAllChannels()
-    {
-        return m_service.GetAllChannels();
-    }
+    // // TODO: the videos within a channel response do not get turned into VideoResponses,
+    // // hence the enums do not appear as text, just an int
+    // [HttpGet("channels")]
+    // public IEnumerable<Channel> GetAllChannels()
+    // {
+    //     return m_service.GetAllChannels();
+    // }
 
-    [HttpPut("channels")]
-    public ActionResult UpdateAllChannels()
-    {
-        var channels = m_service.GetAllChannels();// as List<Channel>;
-        foreach (var ch in channels)
-        {
-            m_service.UpdateChannelSubscriptions(ch);
-        }
-        return Ok();
-    }
+    // [HttpPut("channels")]
+    // public ActionResult UpdateAllChannels()
+    // {
+    //     var channels = m_service.GetAllChannels();// as List<Channel>;
+    //     foreach (var ch in channels)
+    //     {
+    //         m_service.UpdateChannelSubscriptions(ch);
+    //     }
+    //     return Ok();
+    // }
 
-    [HttpPut("channels/{id}")]
-    public ActionResult UpdateChannel(Channel ch)
-    {
-        // TODO: need to distinguish between channels that exist and channels that don't exists with a 200 and 404 respectively
-        m_service.UpdateChannelSubscriptions(ch);
-        return Ok();
-    }
+    // [HttpPut("channels/{id}")]
+    // public ActionResult UpdateChannel(Channel ch)
+    // {
+    //     // TODO: need to distinguish between channels that exist and channels that don't exists with a 200 and 404 respectively
+    //     m_service.UpdateChannelSubscriptions(ch);
+    //     return Ok();
+    // }
 
-    [HttpGet("channels/{id}")]
-    public ActionResult<Channel> GetChannel(string id)
-    {
-        var channel = m_service.GetChannel(id);
-        if (channel is null)
-            return NotFound();
+    // [HttpGet("channels/{id}")]
+    // public ActionResult<Channel> GetChannel(string id)
+    // {
+    //     var channel = m_service.GetChannel(id);
+    //     if (channel is null)
+    //         return NotFound();
         
-        return channel;
-    }
+    //     return channel;
+    // }
 
-    [HttpGet("channels/{id}/subscriptions")]
-    public IEnumerable<VideoResponse> GetChannelSubscriptions(string id)
-    {
-        return m_service.GetChannelVideosByType(id, VideoType.Subscription)
-            .Select(video => new VideoResponse(video));
-    }
+    // [HttpGet("channels/{id}/subscriptions")]
+    // public IEnumerable<VideoResponse> GetChannelSubscriptions(string id)
+    // {
+    //     return m_service.GetChannelVideosByType(id, VideoType.Subscription)
+    //         .Select(video => new VideoResponse(video));
+    // }
 
-    [HttpGet("channels/{id}/watch-later")]
-    public IEnumerable<VideoResponse> GetChannelWatchLaters(string id)
-    {
-        return m_service.GetChannelVideosByType(id, VideoType.WatchLater)
-            .Select(video => new VideoResponse(video));
-    }
+    // [HttpGet("channels/{id}/watch-later")]
+    // public IEnumerable<VideoResponse> GetChannelWatchLaters(string id)
+    // {
+    //     return m_service.GetChannelVideosByType(id, VideoType.WatchLater)
+    //         .Select(video => new VideoResponse(video));
+    // }
 }
