16999169c43116c63ab19d6f55fc4d3aacb0966f
Erik Marsh <erik.i.marsh@gmail.com>
2023-01-07 19:40:42 -0800

    Button callbacks to update channels and subsscribe to channels

--- wwwroot/js/clickCallbacks.js
+++ wwwroot/js/clickCallbacks.js
@@ -4,3 +4,15 @@ const delete_video = (videoId) => {
 
     fetch(`/Video/videos/${videoId}`, { method: "DELETE" });
 };
+
+const update_channel_subscriptions = (channelId) => {
+    let res = fetch(`/Video/channels/${channelId}`, { method: "PUT" });
+    console.log(res);
+};
+
+const subscribe_to_channel = () => {
+    var inputField = document.getElementById("add_subscription_field");
+    let channelId = inputField.value;
+
+    fetch(`/Video/channels/${channelId}`, { method: "POST" });
+};
