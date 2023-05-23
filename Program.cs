f6df41fe10e4d857fb0e5c449518394009070ae2
Erik Marsh <erik.i.marsh@gmail.com>
2023-05-23 00:05:14 -0700

    Let the JSON serializer serialize fields of structs, not just properties

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options => options.JsonSerializerOptions.IncludeFields = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
